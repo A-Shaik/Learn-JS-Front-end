f4f6e653d49e8a1b6d7a36904bf01b561d76c34d
A.Shaik <you@example.com>
2021-05-23 18:50:01 +0300

    home1

---
+++ home1/script.js
@@ -0,0 +1,75 @@
+//test2 console.log ('Shaikovskyi')
+
+/*task3
+let a = 10
+let b = "hometask"
+alert(a)
+alert (b)
+a = b
+alert (a)
+alert (b)
+*/
+
+
+/*task4
+let example
+let user = {
+    
+String : 'AAAA',
+Number : 2222,   
+Boolean: 1>0 ,
+Undefined: example,
+null:null ,
+};
+console.log (user)
+*/
+
+/*task5
+let isAdult = prompt ('А Ви вже повнолітні?', "")
+console.log (isAdult)
+*/
+/* task6
+let myName= 'Anatolii';
+let surname = 'Shaikovskii';
+let studentClass = 602;
+let birthYear = 1986;
+
+let status = 'single';
+
+console.log (typeof myName)
+console.log (typeof surname)
+console.log (typeof studentClass)
+console.log (typeof birthYear)
+console.log (typeof status)
+
+console.log (birthYear)
+console.log (studentClass)
+console.log (myName)
+console.log (surname)
+console.log (status)
+
+let a
+let b
+console.log (a)
+console.log (b)
+
+let c = null
+console.log (typeof c)
+*/
+
+/* task7
+let x = prompt ('Tell me please your Login', '');
+let y = prompt ('Tell me please your email','');
+let z = prompt ('Tell me please your password', '');
+
+alert ('Dear user ' + x + ', your email is ' + y + ', your password is ' + z)
+*/
+
+/*task8
+let hour = 60*60
+let day = 60*60*24
+let mounth = 60*60*24*31
+alert ('В годині ' +hour+ ' секунд')
+alert ('В добі '+day+ ' секунд')
+alert ('В місяці '+ mounth+ ' секунд')
+*/
